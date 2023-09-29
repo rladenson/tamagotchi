954dfa1bab98449880638cefcc08c307bd894c77
rladenson <rachelladenson01@gmail.com>
2023-09-28 19:54:08 -0600

    Make stats

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 class Game {
     constructor() {
-
+        this.hunger = document.getElementById("hunger");
+        this.hunger.textContent = 3;
+        this.sleepiness = document.getElementById("sleepiness");
+        this.sleepiness.textContent = 3;
+        this.boredom = document.getElementById("boredom");
+        this.boredom.textContent = 3;
+        this.age = document.getElementById("age");
+        this.age.textContent = 0;
     }
 }
 
