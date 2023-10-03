a2d7565485945f2a655d4d87da86e802209ca110
Petal Ladenson <habonimkef@gmail.com>
2023-10-02 18:19:10 -0600

    add ability to lock actions from happening

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ class Game {
         this.age = document.getElementById("age");
         this.age.textContent = 0;
 
+        this.lock = false;
         document.getElementById("feed").onclick = this.feed;
         document.getElementById("lights").onclick = this.lights;
         document.getElementById("play").onclick = this.play;
@@ -37,28 +38,34 @@ class Game {
         }, 1000); // 1 sec
     }
     feed = () => {
-        let hunger = this.hunger.textContent;
-        hunger -= 4;
-        if (hunger <= 0)
-            this.hunger.textContent = 0;
-        else
-            this.hunger.textContent = hunger;
+        if (!this.lock) {
+            let hunger = this.hunger.textContent;
+            hunger -= 4;
+            if (hunger <= 0)
+                this.hunger.textContent = 0;
+            else
+                this.hunger.textContent = hunger;
+        }
     }
     lights = () => {
-        let sleepiness = this.sleepiness.textContent;
-        sleepiness -= 10;
-        if (sleepiness <= 0)
-            this.sleepiness.textContent = 0;
-        else
-            this.sleepiness.textContent = sleepiness;
+        if (!this.lock) {
+            let sleepiness = this.sleepiness.textContent;
+            sleepiness -= 10;
+            if (sleepiness <= 0)
+                this.sleepiness.textContent = 0;
+            else
+                this.sleepiness.textContent = sleepiness;
+        }
     }
     play = () => {
-        let boredom = this.boredom.textContent;
-        boredom -= 3;
-        if (boredom <= 0)
-            this.boredom.textContent = 0;
-        else
-            this.boredom.textContent = boredom;
+        if (!this.lock) {
+            let boredom = this.boredom.textContent;
+            boredom -= 3;
+            if (boredom <= 0)
+                this.boredom.textContent = 0;
+            else
+                this.boredom.textContent = boredom;
+        }
     }
     die = () => {
         clearInterval(this.ageInterval);
